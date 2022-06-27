02002e95cddf3da3a919e86cb472e94e58def22c
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-06-27 11:32:55 -0500

    Update asupan-hijaber.js

--- plugins/asupan-hijaber.js
+++ plugins/asupan-hijaber.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/hijaber'
+let api-hyzer = 'https://api.zacros.my.id/asupan/random'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.hijaber', m)
 }
-handler.help = ['hijaber']
+handler.help = ['asupan']
 handler.tags = ['asupan']
-handler.command = /^(hijaber)$/i
+handler.command = /^(asupan)$/i
 
 module.exports = handler
